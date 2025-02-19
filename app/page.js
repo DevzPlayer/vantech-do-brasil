5423202e4ff2fc5da058069aa1d4c2bef42e4b78
Matheus Silva <Sousofriren2022@gmail.com>
2025-02-19 07:50:12 -0400

    Update page.js
    Adicionando verificação

--- app/page.js
+++ app/page.js
@@ -1,15 +1,21 @@
+import Head from 'next/head';
 import Hero from '../components/Hero';
-import Team from '../components/Team'
+import Team from '../components/Team';
 import Services from '../components/Services';
 import ContactForm from '../components/ContactForm';
 
 export default function Home() {
   return (
-    <div>
-      <Hero />
-      <Team />
-      <Services />
-      <ContactForm />
-    </div>
+    <>
+      <Head>
+        <meta name="google-site-verification" content="vRitRtVPNJZBgrM_vn8hGKviBf1bMXHJVhOMAhFCYXA" />
+      </Head>
+      <div>
+        <Hero />
+        <Team />
+        <Services />
+        <ContactForm />
+      </div>
+    </>
   );
 }
